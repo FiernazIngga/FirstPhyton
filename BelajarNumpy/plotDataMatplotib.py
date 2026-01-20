4662e756561df4f8f11faf6190c4a6797c5fe0db
FiernazIngga <inggapratama86@gmail.com>
2026-01-20 09:24:35 +0700

    comit lagi

--- BelajarNumpy/plotDataMatplotib.py
+++ BelajarNumpy/plotDataMatplotib.py
@@ -5,5 +5,20 @@ import matplotlib.pyplot as plt
 os.system("cls")
 
 # Persamaan garis
-x = np.arange(1,10,1)
+x = np.arange(0,11,1)
+y = 2*x + 3
 print(x)
+print(y)
+
+plt.figure(1)
+plt.plot(x,y)
+
+# Lingkaran
+jari2 = 5
+sudut = np.linspace(0,2*np.pi,100)
+x2 = jari2*np.cos(sudut)
+y2 = jari2*np.sin(sudut)
+
+plt.figure(2)
+plt.plot(x2,y2)
+plt.show()
